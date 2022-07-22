b5e2f1f8be64da9bbfd2c181eac0d42787344502
avotcorg <102286982+avotcorg@users.noreply.github.com>
2022-07-22 14:43:36 +0800

    Update l2tp.sh

--- l2tp.sh
+++ l2tp.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-#=======================================================================#
-#   System Supported:  CentOS 6+ / Debian 7+ / Ubuntu 12+               #
-#   Description: L2TP VPN Auto Installer                                #
-#   Author: Teddysun <i@teddysun.com>                                   #
-#   Intro:  https://teddysun.com/448.html                               #
-#=======================================================================#
 cur_dir=`pwd`
 
 libreswan_filename="libreswan-3.27"
@@ -683,8 +677,6 @@ l2tp(){
     echo "###############################################################"
     echo "# L2TP VPN Auto Installer                                     #"
     echo "# System Supported: CentOS 6+ / Debian 7+ / Ubuntu 12+        #"
-    echo "# Intro: https://teddysun.com/448.html                        #"
-    echo "# Author: Teddysun <i@teddysun.com>                           #"
     echo "###############################################################"
     echo
     rootness
